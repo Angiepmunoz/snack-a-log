b3b5d529db272baf9d42bd275f89a4cdf5613807
Angie Munoz <angiepmunoz@gmail.com>
2022-02-11 15:00:43 -0500

    comment

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS snack_a_log;
-CREATE DATABASE snack_a_log; 
+-- DROP DATABASE IF EXISTS snack_a_log;
+-- CREATE DATABASE snack_a_log; 
 
-\c snack_a_log; 
+-- \c snack_a_log; 
 
 
 CREATE TABLE snacks (
